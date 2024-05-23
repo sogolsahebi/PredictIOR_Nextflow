7b78ae12c63be5d3eef9e542daa157423b9baab0
Nasim Bondar Sahebi <sogol.sahebi@mail.utoronto.ca>
2024-05-23 15:35:20 +0000

    Add keep-alive task to Gitpod configuration

---
+++ keep_alive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true; do
+  curl -s https://8787-sogolsahebi-predictiorn-g4zh5pzzmjd.ws-us114.gitpod.io > /dev/null
+  sleep 600 # Wait for 10 minutes before the next request
+done
